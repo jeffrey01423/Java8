e78ec9aa34992a83d2bc56ce6305f7cb69fd8bc3
jeffrey01423 <89950800+jeffrey01423@users.noreply.github.com>
2021-10-24 02:44:56 +0800

    Create Class04.java

---
+++ Class04.java
@@ -0,0 +1,20 @@
+class Cbox{
+	int length;
+	int width;
+	int height;
+}
+public class HW8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cbox box;
+		box=new Cbox();
+		box.length=15;
+		box.width=10;
+		box.height=25;
+		System.out.println("length= "+box.length);
+		System.out.println("width= "+box.width);
+		System.out.println("height= "+box.height);
+	}
+
+}
